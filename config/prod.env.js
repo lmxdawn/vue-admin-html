3aa44b7848fff0b4816f8193c168e59646d0dd03
lmxdawn@qq.com <lmxdawn@qq.com>
2018-07-03 17:35:37 +0800

    清空 v1.0.0 的文件

--- config/prod.env.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-// API_SUFFIX ：API接口后缀
-module.exports = {
-    NODE_ENV: '"production"',
-    API_SUFFIX: '""',
-    API_BASE: '"https://lmxdawn.github.io/vue-admin/data"',
-}
--- src/api/options.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by lk on 17/6/4.
- */
-import fetch from '../utils/fetch'
-
-export const footerTabLists = (cb) => fetch('/static/tempdata/footer.json', {}, 'GET', cb)
